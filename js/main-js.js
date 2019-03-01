6fde5e960bbbc81abb0c453671048f352e30e339
dr1pryaha <dr.pryah@gmail.com>
2019-03-01 13:59:20 +0300

    version 2

---
+++ js/main-js.js
@@ -0,0 +1,17 @@
+let inputField = document.querySelector('.entry-field');
+let sectionList = document.querySelector('.list-event');
+
+let makeElement = function(tagName, className, text){
+	if (inputField === text){
+		let enterHandler = function(){
+			if(evt.keyCode === 13){
+				let div = document.createElement('div');
+				div.className = "to-do-list";
+				div.innerHTML === inputField;
+				sectionList.appendChild(div);
+			}
+		}
+		inputField === undefined;
+	}
+	return sectionList;
+}
