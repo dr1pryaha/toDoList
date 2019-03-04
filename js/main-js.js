799592ab8aa13d80b423301c0930a961e7291796
dr1pryaha <dr.pryah@gmail.com>
2019-03-04 09:55:14 +0300

    Changes enterHandler

--- js/main-js.js
+++ js/main-js.js
@@ -3,7 +3,7 @@ let sectionList = document.querySelector('.list-event');
 
 let makeElement = function(tagName, className, text){
 	if (inputField === text){
-		let enterHandler = function(){
+		let enterHandler = function(evt){
 			if(evt.keyCode === 13){
 				let div = document.createElement('div');
 				div.className = "to-do-list";
