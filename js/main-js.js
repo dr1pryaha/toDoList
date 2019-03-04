83e5e7e643303037eb47cfba1aa4993a660771b2
dr1pryaha <dr.pryah@gmail.com>
2019-03-04 12:45:24 +0300

    Added handler on keydown

--- js/main-js.js
+++ js/main-js.js
@@ -3,14 +3,14 @@ let sectionList = document.querySelector('.list-event');
 
 let makeElement = function(tagName, className, text){
 	if (inputField === text){
-		let enterHandler = function(evt){
+		let enterHandler = document.addEventListener('keydown', function(evt){
 			if(evt.keyCode === 13){
 				let div = document.createElement('div');
 				div.className = "to-do-list";
 				div.innerHTML === inputField;
 				sectionList.appendChild(div);
 			}
-		}
+		});
 		inputField === undefined;
 	}
 	return sectionList;
