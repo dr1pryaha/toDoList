ef1e22a512605081404af91cab2f0a3dfc8ad6d3
dr1pryaha <dr.pryah@gmail.com>
2019-03-06 09:41:49 +0300

    Add keydownHandler

--- js/main-js.js
+++ js/main-js.js
@@ -1,17 +1,54 @@
-let inputField = document.querySelector('.entry-field');
-let sectionList = document.querySelector('.list-event');
+window.onload = function(){
+	let inputField = document.querySelector('div.head-field input[name = entry-field]');
+	let sectionList = document.querySelector('.list-event');
 
-let makeElement = function(tagName, className, text){
-	if (inputField === text){
-		let enterHandler = document.addEventListener('keydown', function(evt){
-			if(evt.keyCode === 13){
-				let div = document.createElement('div');
-				div.className = "to-do-list";
-				div.innerHTML === inputField;
-				sectionList.appendChild(div);
-			}
-		});
-		inputField === undefined;
-	}
-	return sectionList;
+/*Проверка на наличие текста в поле ввода*/
+	function checkText (text) {
+		if (inputField.value.length !== 0){
+			keydownHandler();
+		};
+		else{
+			alert("Введите заметку!");
+		}
+	};
+
+/*Функция создания списка и обработки нажатия клавиши*/
+	let keydownHandler = inputField.addEventListener('keydown', function (evt) {
+		if(evt.keyCode === 13){
+			let div = document.createElement('div');
+			let icon = document.createElement('i');
+			icon.className ="fa-li fa fa-spinner fa-spin";
+			div.className = "to-do-list";
+			div.innerHTML = inputField.value;
+			sectionList.appendChild(div);
+			div.appendChild(icon);
+			inputField.value = '';
+		}
+		return sectionList;
+		//checkText(inputField.value) !!!!!!
+	});
 }
+
+
+
+
+
+
+
+
+/*	let makeElement = function(text){
+		if (inputField === text){
+			let enterHandler =  function(evt){
+				if(evt.keyCode === 13){
+					let div = document.createElement('div');
+					div.className = "to-do-list";
+					div.innerHTML === inputField;
+					sectionList.appendChild(div);
+				}
+			};
+			inputField.onkeydown = enterHandler;
+			inputField === undefined;
+		};
+	return sectionList;
+	};
+};*/
